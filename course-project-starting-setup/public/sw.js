876138e14ae28856ce91d14b727bb417f6fd76e5
Colin McNamara <coolinmc6@gmail.com>
2018-12-18 10:30:35 -0500

    finish L110

--- course-project-starting-setup/public/sw.js
+++ course-project-starting-setup/public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/utility.js');
 
-var CACHE_STATIC_NAME = 'static-v16';
+var CACHE_STATIC_NAME = 'static-v18';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -20,26 +20,6 @@ var STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
-
-// Moved to utility.js
-// var dbPromise = idb.open('posts-store', 1, function(db) {
-//   if(!db.objectStoreNames.contains('posts')) {
-//     db.createObjectStore('posts', {keyPath: 'id'});  
-//   }
-// });
-
-// function trimCache(cacheName, maxItems) {
-//   caches.open(cacheName)
-//     .then(function (cache) {
-//       return cache.keys()
-//         .then(function (keys) {
-//           if (keys.length > maxItems) {
-//             cache.delete(keys[0])
-//               .then(trimCache(cacheName, maxItems));
-//           }
-//         });
-//     })
-// }
 
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -89,7 +69,10 @@ self.addEventListener('fetch', function (event) {
     event.respondWith(fetch(event.request)
       .then(function (res) {
         var clonedRes = res.clone();
-        clonedRes.json()
+        clearAllData('posts')
+          .then(function() {
+            return clonedRes.json()
+          })
           .then(function(data) {
             for (var key in data) {
               writeData('posts', data[key]);
